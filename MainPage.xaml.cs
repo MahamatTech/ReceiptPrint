26c76535f480e4a19ac5d06aa458f3d843e0889e
Mahamat Adoum <88563359+MahamatTech@users.noreply.github.com>
2022-04-25 22:53:27 -0400

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Adoum_FinalProject2022.Model;
+using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Adoum_FinalProject2022
+{
+    
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            dtpEnterPurchaseDate.SelectedDate = DateTime.Now.Date;  //Sets today as a default value for the date picker.
+
+        }
+        private async void btnPrintReceipt_Click(object sender, RoutedEventArgs e)
+        {
+            // We will throw an error message if the cachier forgot to fill out a section
+            if (txtEnterCustomerName.Text == "")
+            {
+
+                MessageDialog messageDialog = new MessageDialog("Please fill out the customer Name.");
+                await messageDialog.ShowAsync();
+
+
+
+            }
+
+            else if (txtEnterProductName.Text == "")
+            {
+                MessageDialog dialog = new MessageDialog("Please fill out Product Name.");
+                await dialog.ShowAsync();
+
+
+            }
+
+
+            else if (txtEnterTaxRate.Text == "")
+            {
+                MessageDialog dialog = new MessageDialog("Please include the tax Rate");
+                await dialog.ShowAsync();
+
+            }
+
+
+            else if (txtEnterPrices.Text == "")
+            {
+                MessageDialog dialog = new MessageDialog("The price section is Empty");
+                await dialog.ShowAsync();
+
+
+            }
+
+
+            else
+            {
+
+                // This code obtains the user input from controls
+                string customerName = txtEnterCustomerName.Text;
+                string productName = txtEnterProductName.Text;
+                DateTime purchaseDate = dtpEnterPurchaseDate.Date.DateTime;
+                decimal taxPercent = decimal.Parse(txtEnterTaxRate.Text);
+                decimal[] purchases = ConvertPurchasesToArray(txtEnterPrices.Text);
+                string userName = txtEnterCustomerName.Text;
+                string userProduct = txtEnterProductName.Text; 
+
+
+                Receipt receipt = new Receipt(userName, userProduct, purchaseDate, purchases);
+                txtReceiptStoreName.Text = receipt.StoreName;
+                txtReceiptTotal.Text = Math.Round(receipt.CalculateTotal(taxPercent), 2).ToString();
+                txtReceiptPurchaseValues.Text = receipt.ListItemPrices().ToString();
+
+                txtReceiptCustomerName.Text = receipt.CustomerName;
+                txtReceiptProductName.Text = receipt.ProductName;
+                txtReceiptPurchaseDate.Text = receipt.PurchaseDate;
+                //This line of code make the display receipt section visible
+                DisplayReceipt.Visibility = Visibility.Visible;
+
+
+            }
+
+
+
+
+        }
+
+        private decimal[] ConvertPurchasesToArray(string valuesList)
+        {
+            try
+            {
+                string[] tempValues = valuesList.Split(new char[] { '\r', '\n', ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                decimal[] priceValues = Array.ConvertAll(tempValues, Convert.ToDecimal);
+                return priceValues;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            //Clear the user input from the Point of Sale Terminal 
+            txtEnterCustomerName.Text = "";
+            txtEnterProductName.Text = "";
+            dtpEnterPurchaseDate.SelectedDate = DateTime.Today.Date;
+            txtEnterTaxRate.Text = "0.15";
+            txtEnterPrices.Text = "";
+            // Hide the display receipt section
+            DisplayReceipt.Visibility = Visibility.Collapsed;
+        }
+
+        private void txtEnterPrices_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtReceiptPurchaseValues_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Model/Receipt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adoum_FinalProject2022.Model
+{
+    class Receipt
+    {
+
+        public static string storeName = "Birreh Import-Export";
+        private string customerName;
+        private string productName;
+        private decimal[] itemPrices;
+        private DateTime purchaseDate;
+
+        public string CustomerName
+        {
+            get { return customerName; }
+        }
+
+
+        public string ProductName
+        {
+            get { return productName; }
+        }
+
+        public string StoreName
+        {
+            get { return storeName; }
+        }
+
+        public string PurchaseDate
+        {
+            get { return $"{purchaseDate.Year}, {purchaseDate.Month}, {purchaseDate.Day}"; }
+        }
+
+        // List item prices
+        public string ListItemPrices()
+        {
+            string listOfPrices = "";
+            foreach (var i in itemPrices)
+            {
+                listOfPrices += $"{i:C} \n";
+            }
+            return listOfPrices;
+        }
+
+
+        public decimal CalculateTotal(decimal taxRate)
+        {
+            decimal total = 0.00m;
+            foreach (var i in itemPrices)
+            {
+                total += i;
+            }
+            return (total * taxRate) + total;
+        }
+
+        public Receipt(string CustomerName, string ProductName, DateTime PurchaseDate, decimal[] ItemPrices)
+        {
+            customerName = CustomerName;
+            productName = ProductName;
+            purchaseDate = PurchaseDate;
+            itemPrices = ItemPrices;
+        }
+    }
+}
